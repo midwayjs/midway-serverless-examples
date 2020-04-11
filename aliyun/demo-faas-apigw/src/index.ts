c90abc6e1200f19d568e00fea6e177fd530b4506
张挺 <zhangting@taobao.com>
2020-04-12 01:19:40 +0800

    chore: update case

--- aliyun/demo-faas-apigw/src/index.ts
+++ aliyun/demo-faas-apigw/src/index.ts
@@ -1,5 +1,5 @@
 import { Func, Inject, Provide } from '@midwayjs/decorator';
-import { FaaSContext, FunctionHandler } from '@midwayjs/faas';
+import { FaaSContext, FunctionHandler, FC } from '@midwayjs/faas';
 
 @Provide()
 @Func('index.handler')
@@ -8,7 +8,12 @@ export class IndexService implements FunctionHandler {
   @Inject()
   ctx: FaaSContext;  // context
 
-  async handler() {
-    return 'hello world';
+  async handler(event: FC.APIGatewayEvent) {
+    return {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: 'hello world',
+    } as FC.APIGatewayResponse;
   }
 }
