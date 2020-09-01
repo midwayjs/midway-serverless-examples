9b1f47b17d760e4414721bea9896359f30942888
echosoar <82163514@qq.com>
2020-09-01 12:00:08 +0800

    fix: vue2

--- integration/vue2/boilerplate/src/App.vue
+++ integration/vue2/boilerplate/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld
-      msg="Welcome to Your Vue.js + Midway FaaS App"
+      msg="Welcome to Your Vue.js + Midway App"
       faasResponse="FaaS API Loading..."
     />
   </div>
