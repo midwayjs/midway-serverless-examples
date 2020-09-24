7a91c4c987fc6cb36ac44ba8206a934ba3568c06
新影 <xinying.ykk@alibaba-inc.com>
2020-09-24 14:19:19 +0800

    feat: add hooks-vue3-demo

---
+++ integration/hooks-vue3/README.md
@@ -0,0 +1,5 @@
+# Midway Serverless with Vue3
+
+Midway Serverless hooks + Vue3 一体化开发脚手架
+
+![](https://gw.alicdn.com/tfs/TB1B9phR.z1gK0jSZLeXXb9kVXa-2880-1800.png)
---
+++ integration/hooks-vue3/boilerplate/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="message" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { hello } from '../apis/lambda';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  message = '';
+
+  created() {
+    hello().then((res) => {
+      console.log(res);
+      this.message = res.message;
+    });
+  }
+}
+</script>
