a3ff28a5165476f79e64d669392c2ee355cda5db
yincoco <524404942@qq.com>
2020-09-25 15:56:08 +0800

    fix: remove no-multiple-template-root

--- integration/hooks-vue3/boilerplate/.eslintrc.js
+++ integration/hooks-vue3/boilerplate/.eslintrc.js
@@ -11,6 +11,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 'off',
-    'vue/no-multiple-template-root': 'off',
   },
 };
